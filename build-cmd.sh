c83a6baed4b839bad6e0caa8a861906f1c4ceb88
Rye <rye@alchemyviewer.org>
2024-11-17 23:09:38 -0500

    Update to SDL 3.2.4
    Add macOS universal library build support
    
    Switch to SCM versioning
    
    Update GHA workflow to use alchemy actions

--- build-cmd.sh
+++ build-cmd.sh
@@ -19,7 +19,7 @@ fi
 
 TOP="$(dirname "$0")"
 
-SDL_SOURCE_DIR="SDL2"
+SDL_SOURCE_DIR="SDL3"
 
 stage="$(pwd)"
 
@@ -47,7 +47,7 @@ case "$AUTOBUILD_PLATFORM" in
     windows*)
         load_vsvars
 
-        mkdir -p "$stage/include/SDL2"
+        mkdir -p "$stage/include/SDL3"
         mkdir -p "$stage/lib/debug"
         mkdir -p "$stage/lib/release"
 
@@ -81,50 +81,63 @@ case "$AUTOBUILD_PLATFORM" in
 
             cp $stage/release/bin/*.dll $stage/lib/release/
             cp $stage/release/lib/*.lib $stage/lib/release/
-            cp $stage/release/include/SDL2/*.h $stage/include/SDL2/
+            cp $stage/release/include/SDL3/*.h $stage/include/SDL3/
         popd
     ;;
     darwin*)
-        # Setup osx sdk platform
         export MACOSX_DEPLOYMENT_TARGET="$LL_BUILD_DARWIN_DEPLOY_TARGET"
 
-        # Setup build flags
-        opts="${TARGET_OPTS:--arch $AUTOBUILD_CONFIGURE_ARCH $LL_BUILD_RELEASE}"
+        for arch in x86_64 arm64 ; do
+            ARCH_ARGS="-arch $arch"
+            opts="${TARGET_OPTS:-$ARCH_ARGS $LL_BUILD_RELEASE}"
+            cc_opts="$(remove_cxxstd $opts)"
+            ld_opts="$ARCH_ARGS"
 
-        mkdir -p "$stage/include/SDL2"
-        mkdir -p "$stage/lib/release"
+            if [ "$arch" = "x86_64" ] ; then
+                SDL_CPU_X64="1"
+                SDL_CPU_ARM64="0"
+            else
+                SDL_CPU_X64="0"
+                SDL_CPU_ARM64="1"
+            fi
 
-        PREFIX_RELEASE="$stage/temp_release"
-        mkdir -p $PREFIX_RELEASE
+            mkdir -p "build_$arch"
+            pushd "build_$arch"
+                CFLAGS="$cc_opts" \
+                CXXFLAGS="$opts" \
+                LDFLAGS="$ld_opts" \
+                cmake .. -G Ninja -DCMAKE_BUILD_TYPE="Release" -DCMAKE_CONFIGURATION_TYPES="Release" \
+                    -DCMAKE_C_FLAGS="$cc_opts" \
+                    -DCMAKE_CXX_FLAGS="$opts" \
+                    -DCMAKE_OSX_ARCHITECTURES:STRING="$arch" \
+                    -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET} \
+                    -DCMAKE_MACOSX_RPATH=YES \
+                    -DCMAKE_INSTALL_PREFIX="$stage" \
+                    -DCMAKE_INSTALL_LIBDIR="$stage/lib/release/$arch" \
+                    -DSDL_CPU_ARM64="$SDL_CPU_ARM64" \
+                    -DSDL_CPU_X64="$SDL_CPU_X64"
 
-        mkdir -p "build_release"
-        pushd "build_release"
-            cmake .. -GNinja -DCMAKE_BUILD_TYPE="Release" \
-                -DCMAKE_C_FLAGS="$(remove_cxxstd $opts)" \
-                -DCMAKE_CXX_FLAGS="$opts" \
-                -DCMAKE_OSX_ARCHITECTURES:STRING=x86_64 \
-                -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET} \
-                -DCMAKE_MACOSX_RPATH=YES \
-                -DCMAKE_INSTALL_PREFIX=$PREFIX_RELEASE
+                cmake --build . --config Release
+                cmake --install . --config Release
+            popd
+        done
 
-            cmake --build . --config Release
-            cmake --install . --config Release
-        popd
 
-        cp -a $PREFIX_RELEASE/include/SDL2/*.* $stage/include/SDL2
-        cp -a $PREFIX_RELEASE/lib/*.dylib* $stage/lib/release
-        cp -a $PREFIX_RELEASE/lib/libSDL2main.a $stage/lib/release
+        # create universal libraries
+        lipo -create -output ${stage}/lib/release/libSDL3.dylib ${stage}/lib/release/x86_64/libSDL3.dylib ${stage}/lib/release/arm64/libSDL3.dylib
+        lipo -create -output ${stage}/lib/release/libSDL3_test.a ${stage}/lib/release/x86_64/libSDL3_test.a ${stage}/lib/release/arm64/libSDL3_test.a
 
         pushd "${stage}/lib/release"
-            fix_dylib_id "libSDL2.dylib"
-            strip -x -S libSDL2.dylib
+            install_name_tool -id "@rpath/libSDL3.dylib" "libSDL3.dylib"
+            dsymutil libSDL3.dylib
+            strip -x -S libSDL3.dylib
         popd
         ;;
     linux*)
         # Default target per autobuild build --address-size
         opts="${TARGET_OPTS:--m$AUTOBUILD_ADDRSIZE $LL_BUILD_RELEASE}"
 
-        mkdir -p "$stage/include/SDL2"
+        mkdir -p "$stage/include/SDL3"
         mkdir -p "$stage/lib/release"
 
         PREFIX_RELEASE="$stage/temp_release"
@@ -141,9 +154,9 @@ case "$AUTOBUILD_PLATFORM" in
             cmake --install . --config Release
         popd
 
-        cp -a $PREFIX_RELEASE/include/SDL2/*.* $stage/include/SDL2
+        cp -a $PREFIX_RELEASE/include/SDL3/*.* $stage/include/SDL3
         cp -a $PREFIX_RELEASE/lib/*.so* $stage/lib/release
-        cp -a $PREFIX_RELEASE/lib/libSDL2main.a $stage/lib/release
+        cp -a $PREFIX_RELEASE/lib/libSDL3main.a $stage/lib/release
     ;;
 
     *)
@@ -152,8 +165,5 @@ case "$AUTOBUILD_PLATFORM" in
 esac
 popd
 
-SDL_VERSION=$(sed -n -e 's/^Version: //p' "$TOP/$SDL_SOURCE_DIR/build_release/SDL2.spec")
-
 mkdir -p "$stage/LICENSES"
-cp "$TOP/$SDL_SOURCE_DIR/LICENSE.txt" "$stage/LICENSES/SDL2.txt"
-echo "$SDL_VERSION" > "$stage/VERSION.txt"
+cp "$TOP/$SDL_SOURCE_DIR/LICENSE.txt" "$stage/LICENSES/SDL3.txt"
